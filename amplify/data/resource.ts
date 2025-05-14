044c4b5fd95ec16cc2e93bc71cb6638db07a92fd
Xin Qiao <qiaoxin136@gmail.com>
2025-05-14 10:57:04 -0400

    add track

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,7 @@ const schema = a.schema({
       content: a.string(),
       date: a.date(), 
       time: a.time(), 
+      track: a.integer(),
       type: a.string(), 
       status: a.string(), 
       lat: a.float(),
